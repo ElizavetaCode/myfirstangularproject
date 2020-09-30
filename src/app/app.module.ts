9cba53d52e8aeaa816983202f704fb24fe3b918d
Elizaveta Sh <shestakovaliza@gmail.com>
2020-09-30 11:56:28 +0700

    Add product-details. Working!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { LkComponent } from './components/lk/lk.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProductAlertComponent } from './components/product-alert/product-alert.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 
 const appRoutes: Routes = [
   {path: '', component:CarComponent},
   {path: 'about', component:KontaktiComponent},
   {path: 'lk', component:LkComponent},
-  {path: 'product-list', component:ProductListComponent}
+  {path: 'product-list', component:ProductListComponent},
+  {path: 'products/:productId', component: ProductDetailsComponent},
 ]
 
 @NgModule({
@@ -26,7 +28,8 @@ const appRoutes: Routes = [
     KontaktiComponent,
     LkComponent,
     ProductListComponent,
-    ProductAlertComponent
+    ProductAlertComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/products.ts
+++ src/app/components/products.ts
@@ -7,12 +7,12 @@ export const products = [
   },
   {
     name: 'Phone Mini',
-    price: 699,
+    price: 299,
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standard',
-    price: 299,
+    price: 499,
     description: ''
   }
 ];
